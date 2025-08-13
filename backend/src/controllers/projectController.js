ef5fb0677315827a91428937ee6e1e93a1f532c9
Deepakpaidakula <paidakuladeepak@gmail.com>
2025-08-13 22:32:54 +0530

    Add controllers to handle auth and project logic

---
+++ backend/src/controllers/projectController.js
@@ -0,0 +1,23 @@
+const Project = require("../models/Project");
+
+exports.createProject = async (req, res) => {
+  try {
+    const project = await Project.create({
+      name: req.body.name,
+      description: req.body.description,
+      user: req.user.id
+    });
+    res.json(project);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.getProjects = async (req, res) => {
+  try {
+    const projects = await Project.find({ user: req.user.id });
+    res.json(projects);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
